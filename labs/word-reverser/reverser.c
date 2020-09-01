38da837b6aa8204002f8214db37f543d1379a8ab
Francisco M Amézquita R <A01634495@itesm.mx>
2020-08-31 22:25:25 -0500

    solved word-reverser
    Signed-off-by: Francisco M Amézquita R <A01634495@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    // Place your magic here
+    char c;
+    int numChar = 0;
+    char arrayChar[500];
+
+    while((c = getchar()) != EOF ){
+
+        if(c == '\n'){
+            numChar--;
+            //printf("Reversed word: ");
+            while(numChar >= 0){                
+                printf("%c",arrayChar[numChar]);
+                numChar--;
+            }
+            numChar = 0;
+            printf("\n");
+
+        }
+        else{
+            arrayChar[numChar] = c;
+            numChar++;
+        }
+
+    }
     return 0;
 }
