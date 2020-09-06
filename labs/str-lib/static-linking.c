4a10ff173462edc778047144b7468ed228a6f733
Francisco M Amézquita R <A01634495@itesm.mx>
2020-09-06 18:13:11 -0500

    Solved str-lib
    Signed-off-by: Francisco M Amézquita R <A01634495@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
 
-int main() {
-    return 0;
+int main(int argc, char **argv) {
+
+
+	if(argc < 4){
+		printf("Input invalid, more arguments needed\n");
+		printf("Format as follows: ./xxxx -add 'String1' 'String2' OR ./xxxx -find 'String' 'Substring'\n");
+	} else{
+		if(strcmp(argv[1],"-add") == 0){
+			printf("Initial Length	: %d\n", mystrlen(argv[2]));
+			printf("New String      : %s\n", mystradd(argv[2], argv[3]));
+			printf("New Length 	    : %d\n", mystrlen(mystradd(argv[2], argv[3])));
+		}
+		else if(strcmp(argv[1],"-find") == 0){
+			if(mystrfind(argv[2],argv[3]) != -1){
+				printf("['%s'] string was found at %d position\n", argv[3], mystrfind(argv[2], argv[3]));
+			} else{
+				printf("['%s'] string was not found \n", argv[3]);
+			}
+		}
+	}
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-
+//count length of string
 int mystrlen(char *str){
-    return 0;
+    int count = 0;
+    while(str[count] != NULL){
+    	count++;
+    }
+    return count;
 }
 
+//concat
 char *mystradd(char *origin, char *addition){
-    return 0;
+	int originSize = mystrlen(origin);
+	int additionSize = mystrlen(addition);
+    char *concatStr = malloc(originSize + additionSize);
+
+    //first string
+    for (int i = 0; i < originSize; i++){
+    	concatStr[i] = origin[i];
+    }
+    //second string
+    int i = 0;
+    for(int j = originSize; j < (originSize + additionSize); j++){
+    	concatStr[j] = addition[i];
+    	i++;
+    }
+    return concatStr;
 }
 
+//find substring
 int mystrfind(char *origin, char *substr){
-    return 0;
+
+	int originSize = mystrlen(origin);
+	int subSize = mystrlen(substr);
+
+	for(int i = 0; i < originSize; i++){
+
+		//if the first letter of the substring is found while iterating the string
+		if(origin[i] == substr[0]){
+			//j is used for iterating the string
+			//k is used for iterating the substring
+			int j,k;
+			for(j = i, k = 0; j < originSize && k < subSize; j++, k++){
+				if(origin[j] != substr[k]){
+					break;
+				}
+			}
+			//if the number of iterations is the same as the size of substring
+			//found
+			if(k == subSize){
+				return i;
+			}
+		}
+	}
+
+    return -1;
 }
