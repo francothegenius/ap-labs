6dffc4e0a3f9faef46e34a2f91aecc5dd7c2bd9c
Francisco M Amézquita R <A01634495@itesm.mx>
2020-09-22 11:18:59 -0500

    go-slices-maps solved
    Signed-off-by: Francisco M Amézquita R <A01634495@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+	//"fmt"
+)
+
+func WordCount(s string) map[string]int {
+	sentArr := strings.Fields(s)
+	//fmt.Println(sentArr);
+	m := make(map[string]int)
+	//not assinging index
+	//range goes with respect sentArr
+	//v takes each word
+	for _, v := range sentArr {
+		m[v]+=1
+	}
+	return m
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -3,7 +3,14 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	pic := make([][]uint8,dy)
+	for i:=0; i<dy; i++ {
+		pic[i] = make([]uint8,dx)
+		for j:=0; j<dx; j++ {
+			pic[i][j] = uint8(i*j)
+		}
+	}
+	return pic
 }
 
 func main() {
