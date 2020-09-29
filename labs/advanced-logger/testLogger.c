69ccc6147d5c92a5528db050274f2f8ae778a9b1
Francisco M Amézquita R <A01634495@itesm.mx>
2020-09-29 13:36:03 -0500

    solved advanced logger
    Signed-off-by: Francisco M Amézquita R <A01634495@itesm.mx>

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -3,7 +3,10 @@
 #include "logger.h"
 
 int main(int argc, char **argv){
-
+	if(argc < 2){
+		errorf("Invalid test case\n");
+		return 0;
+	}
     printf("%d\n", atoi(argv[1]));
     switch(atoi(argv[1])) {
     case 1:
@@ -13,22 +16,22 @@ int main(int argc, char **argv){
 	errorf("ERROR Message %d", 2);
 	break;
     case 2:
-	// stdout logging
-	initLogger("stdout");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
+		// stdout logging
+		initLogger("stdout");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
     case 3:
-	// syslog logging
-	initLogger("syslog");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
+		// syslog logging
+		initLogger("syslog");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
     default:
-	errorf("Invalid test case");
-	break;
+		errorf("Invalid test case");
+		break;
     }
 
     return 0;
