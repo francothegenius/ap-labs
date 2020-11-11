7f8df8d3b0bce72697c73a143567c81c497ead69
Francisco M Amezquita R <A01634495@itesm.mx>
2020-11-10 21:13:24 -0600

    go vs threads solved
    Signed-off-by: Francisco M Amezquita R <A01634495@itesm.mx>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func connect(in chan int, out chan int) {
+	for {
+		out <- (1 + <-in)
+	}
+}
+
 func main() {
-	var commsPerSecond int
+	in := make(chan int)
+	out := make(chan int)
+	commsPerSecond := 0
+
+	go connect(in, out)
+	go connect(out, in)
+
+	for i := 0; i < 1; i++ {
+		in <- 0
+		time.Sleep(time.Duration(1) * time.Second)
+		x := <-in
+		commsPerSecond += x
+	}
 
 	fmt.Println("Communications Per Second : ", commsPerSecond)
 }
