aabaf9b57e2c16b72c1f2245d8e15dc875c02822
Francisco M Amezquita R <A01634495@itesm.mx>
2020-11-10 21:12:06 -0600

    go vs threads solved
    Signed-off-by: Francisco M Amezquita R <A01634495@itesm.mx>

--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+var stageMax int
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+func main() {
+	in := make(chan int)
+	out := make(chan int)
+
+	go connect(in, out, 0, 1000000)
+	start := time.Now()
+	in <- 1
+
+	fmt.Println("Maximum number of pipeline stages   : ", stageMax)
+	fmt.Println("Time to transit trough the pipeline : ", time.Since(start))
+}
+
+func connect(in <-chan int, out chan int, index int, connections int) {
+	if index != connections {
+		stageMax++
+		connect(in, out, index+1, connections)
+		out <- <-in
+	}
 }
